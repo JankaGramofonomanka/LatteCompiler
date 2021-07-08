31e663313a683921de501781229b7179432ac5bf
JankaGramofonomanka <tsrn666@gmail.com>
2021-07-08 19:16:57 +0200

    Make fake frontend

--- frontend/Main.hs
+++ frontend/Main.hs
@@ -1,6 +1,28 @@
 module Main where
 
-import Lib
+import System.Environment (getArgs)
+import System.IO
 
-main :: IO ()
-main = someFunc
+
+  
+
+main :: IO Int
+main = do
+
+  args <- getArgs
+
+  let filename = head args
+
+  fileCts <- readFile filename
+
+  let okExitCode = 0
+  let okOutput = "OK\n"
+
+  let errExitCode = 1
+  let errOutput = "ERROR\n fake error" 
+
+
+  hPutStrLn stderr okOutput
+
+  return okExitCode
+
