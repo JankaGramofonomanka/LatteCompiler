fba87c39a89e4c7b1c017e19c1cfbb93ee43505e
JankaGramofonomanka <tsrn666@gmail.com>
2021-07-29 16:33:49 +0200

    Add expressions instructions

--- src/LLVM/LLVM.hs
+++ src/LLVM/LLVM.hs
@@ -46,11 +46,21 @@ instance SingI t => SingI ('Ptr t) where
 instance (SingI n, SingI t) => SingI ('Arr n t) where
   sing = SArr sing sing
 
+type Reg :: PrimType -> Type 
+data Reg t where
+  Reg :: String -> Reg t
+
+type Constant :: PrimType -> Type
+data Constant t where
+  Const :: String -> Constant t
 
 type Value :: PrimType -> Type
 data Value t where
-  Reg :: String -> Value t
-  IConst :: Int -> Value (I n)
+  Var :: Reg t -> Value t
+  ILit :: Int -> Value (I n)
+  ConstPtr :: Constant t -> Value (Ptr t)
+
+newtype Label = Label Int
 
 type FuncLabel :: PrimType -> [PrimType] -> Type
 data FuncLabel t ts where
@@ -71,6 +81,60 @@ data Func t ts where
         -> ArgList ts
         -> FuncLabel t ts
         -> Func t ts
+
+
+type BinOp :: PrimType -> Type
+data BinOp t where
+  ADD   :: BinOp (I n)
+  SUB   :: BinOp (I n)
+  MUL   :: BinOp (I n)
+  SDIV  :: BinOp (I n)
+  UDIV  :: BinOp (I n)
+  SREM  :: BinOp (I n)
+  UREM  :: BinOp (I n)
+
+type BitOp :: PrimType -> Type
+data BitOp t where
+  SHL   :: BitOp (I n)
+  LSHR  :: BitOp (I n)
+  ASHR  :: BitOp (I n)
+  AND   :: BitOp (I n)
+  OR    :: BitOp (I n)
+  XOR   :: BitOp (I n)
+
+
+data CMPKind = EQ | NE | SGT | SGE | SLT | SLE | UGT | UGE | ULT | ULE
+
+
+type Expr :: PrimType -> Type
+data Expr t where
+  BinOperation :: BinOp t -> Value t -> Value t -> Expr t
+  Call :: FuncLabel t ts -> ArgList ts -> Expr t
+  GetElemPtr :: Value (Ptr t) -> Value (I n) -> Expr (Ptr t)
+  ICMP :: CMPKind -> Value (I n) -> Value (I n) -> Expr (I 1)
+
+
+data SimpleInstr where
+  Ass :: Reg t -> Expr t -> SimpleInstr
+  VoidExpr :: Expr 'Void -> SimpleInstr
+
+data BranchInstr where
+  Branch :: Label -> BranchInstr
+  CondBranch :: Value (I 1) -> Label -> Label -> BranchInstr
+  Ret :: Value t -> BranchInstr
+  RetVoid :: BranchInstr
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
